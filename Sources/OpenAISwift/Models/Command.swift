e1ffd37e60f0074e5521c77ea84f2fae9afb0bc7
Neel Virdy <neelvirdy@gmail.com>
2023-05-25 15:06:56 -0400

    Add logitBias, frequencyPenalty, and stop for completion

--- Sources/OpenAISwift/Models/Command.swift
+++ Sources/OpenAISwift/Models/Command.swift
@@ -9,11 +9,23 @@ struct Command: Encodable {
     let model: String
     let maxTokens: Int
     let temperature: Double
+
+    /// Modify the likelihood of specified tokens appearing in the completion. Maps tokens (specified by their token ID in the OpenAI Tokenizer—not English words) to an associated bias value from -100 to 100. Values between -1 and 1 should decrease or increase likelihood of selection; values like -100 or 100 should result in a ban or exclusive selection of the relevant token.
+    let logitBias: [Int: Double]?
+
+    /// A parameter that penalizes new tokens based on their existing frequency in the text so far, between -2 and 2. Positive values decrease the model's likelihood to repeat the same line verbatim. Optional if not specified by default or by user input. Optional, defaults to 0.
+    let frequencyPenalty: Double?
+
+    /// An array of up to 4 sequences where the API will stop generating further tokens. Optional.
+    let stop: [String]?
     
     enum CodingKeys: String, CodingKey {
         case prompt
         case model
         case maxTokens = "max_tokens"
         case temperature
+        case logitBias = "logit_bias"
+        case frequencyPenalty = "frequency_penalty"
+        case stop
     }
 }
